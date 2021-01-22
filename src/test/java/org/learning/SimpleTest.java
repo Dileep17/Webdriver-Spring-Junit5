e696555cd8fddbb7adfa78c7de68cf138f358776
Dileep B <dileep.srimani@gmail.com>
2021-01-22 12:29:52 +0530

    add junit5 to project

---
+++ src/test/java/org/learning/SimpleTest.java
@@ -0,0 +1,13 @@
+package org.learning;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+
+    @Test
+    void pass(){
+        Assertions.assertTrue(true);
+    }
+
+}
